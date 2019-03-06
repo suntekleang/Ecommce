7d04409381690225e32ef06a6b552e43f9c0441d
suntekleang <suntekleang777@gmail.com>
2019-03-06 18:56:22 +0700

    upload

--- src/app/dialog/app.dialog.ts
+++ src/app/dialog/app.dialog.ts
@@ -1,6 +1,10 @@
 import { DeleteComponent } from './../components/delete/delete.component';
 import { AddCategoryComponent } from './add-category/add-category.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { UploadCoverComponent } from './upload-cover/upload-cover.component';
 export const APP_DIALOG=[
   AddCategoryComponent,
-  DeleteComponent
+  DeleteComponent,
+  AddProductComponent,
+  UploadCoverComponent
 ]
--- src/app/store/app.store.ts
+++ src/app/store/app.store.ts
@@ -1,6 +1,8 @@
 import { Category } from './category.store';
 import { User } from './user.store';
+import { Product } from './product.store';
 export const APP_STORE=[
   Category,
-  User
+  User,
+  Product
 ]
